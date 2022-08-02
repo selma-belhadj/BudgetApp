ccba7d459658298ddddb30004ae767536405274c
selma <belhadjselma1@gmail.com>
2022-08-02 19:33:12 +0100

    upadte the configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :deals
   resources :categories
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'users#index'
 
   # Defines the root path route ("/")
   # root "articles#index"
