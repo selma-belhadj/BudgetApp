0427f5ef9502a5c1aa463d16dd51bf00516dea4f
selma <belhadjselma1@gmail.com>
2022-08-03 01:11:21 +0100

    Fix the author_id & user_id conflicts

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User'
   has_many :deals, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
