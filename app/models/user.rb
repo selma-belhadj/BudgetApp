a0f5214abf49f9b7135bfd5c0234802e8472c9f9
selma <belhadjselma1@gmail.com>
2022-08-04 09:40:21 +0100

    add the total amout for all categories for a given user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   has_many :categories, dependent: :destroy
   has_many :deals, dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 }
+
+  def total_amount
+    sum = 0
+    categories.each do |category|
+      category.deals.each do |deal|
+        sum += deal.amount
+      end
+    end
+    sum
+  end
 end
